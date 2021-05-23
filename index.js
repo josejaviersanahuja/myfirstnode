4d55b48b7935de5836243d95ecff628e3a7f26c1
josejavier <82359964+josejaviersanahuja@users.noreply.github.com>
2021-05-23 14:18:30 +0100

    small-description-added

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(cors()) // por defecto, habilita a todo el mundo a usar nuestra API
 app.use(express.json()) // esta es la forma de usar el parser que ofrece expres. Es un middleware
 app.use(logger)
 
-app.get('/', (request, response) => { response.send('<h1>Bienvenido a la primera API de JJ</h1') })
+app.get('/', (request, response) => { response.send(' <h1>Bienvenido a la primera API de ZitrojjDev</h1> <hr/> <h2>Existen de momento 2 llamadas posibles a esta API</h2> <ol><li><b>/api/pokemons</b> que devuelve un json con 1 array de todos los pokemons</li><li><b>/api/pokemon/:id</b> Ten en cuenta que el id es un número que define a un pokemon. esta llamada devuelve un json con un objeto con detalles y datos del pokemon en cuestión</li></ol>') })
 
 app.get('/api/pokemons', (request, response) => { response.json(pokemonsbd) })
 app.get('/api/pokemon/:id', (request, response) => {
